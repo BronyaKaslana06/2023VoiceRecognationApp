9230af12416216d5d8646a6ba187662ca2043506
BronyaKaslana06 <2268920184@qq.com>
2024-01-20 00:04:56 +0800

    ccr new init

--- android/SherpaNcnn/app/src/main/java/com/k2fsa/sherpa/ncnn/SherpaNcnn.kt
+++ android/SherpaNcnn/app/src/main/java/com/k2fsa/sherpa/ncnn/SherpaNcnn.kt
@@ -126,6 +126,7 @@ to add more pre-trained models
 fun getModelConfig(type: Int, useGPU: Boolean): ModelConfig? {
     when (type) {
         0 -> {
+            //val modelDir = "sherpa-ncnn-2022-09-30"
             val modelDir = "sherpa-ncnn-2022-09-30"
             return ModelConfig(
                 encoderParam = "$modelDir/encoder_jit_trace-pnnx.ncnn.param",
